2b8b6caf7c25146f3327aaf075067e5746c9b915
Brian on VM <bvalette@student.42.fr>
2020-12-10 11:07:09 +0100

    [Thu-10.12-11:07] [DEV] fix os issues [NEXT]

--- bvalette_tester/bvalette_philo_one_test.sh
+++ bvalette_tester/bvalette_philo_one_test.sh
@@ -1,14 +1,33 @@
+#! /bin/bash
+
 print_victim_last_meal (){
  time_to_die=$1
  nb_of_victim=$(cat /tmp/a | grep "died" | wc -l)
- victim=$(cat /tmp/a | grep "died" | eval "$SED_CMD" | tail -n2 | head -n1)
+ if [[ "$OSTYPE" == "darwin"* ]]; then
+       victim=$(cat /tmp/a | grep "died" | sed -e $'s/ /\\\n/g' | tail -n2 | head -n1)
+ else
+       victim=$(cat /tmp/a | grep "died" | sed 's/ /\n/g' | tail -n2 | head -n1)
+ fi
+
  echo "Last meal was :"
  cat /tmp/a | grep " $victim is eating" | tail -n1
- time_of_meal=$(cat /tmp/a | grep " $victim is eating" | tail -n1 | eval "$SED_CMD" | head -n1)
+ if [[ "$OSTYPE" == "darwin"* ]]; then
+       time_of_meal=$(cat /tmp/a | grep " $victim is eating" | tail -n1 | sed -e $'s/ /\\\n/g' | head -n1)
+ else
+       time_of_meal=$(cat /tmp/a | grep " $victim is eating" | tail -n1 | sed 's/ /\n/g' | head -n1)
+ fi
+
  echo "Timestamp of the last meal = $time_of_meal"
- time_of_death=$(cat /tmp/a | grep "died" | tail -n1 | eval "$SED_CMD" | head -n1)
+
+ if [[ "$OSTYPE" == "darwin"* ]]; then
+       time_of_death=$(cat /tmp/a | grep "died" | tail -n1 | sed -e $'s/ /\\\n/g' | head -n1)
+ else
+       time_of_death=$(cat /tmp/a | grep "died" | tail -n1 | sed 's/ /\n/g' | head -n1)
+ fi
+
  echo "Timestamp of the death     = $time_of_death"
  echo "Time to die parameter      = $time_to_die"
+
  if [ $(($time_of_meal)) -eq "0" ]; then
         echo "Death reported afer $(( $time_of_death - $time_to_die)) ms."
         echo "Nb of died philosopher: [$nb_of_victim]"
@@ -16,6 +35,7 @@ print_victim_last_meal (){
         echo "Death reported afer $(( $time_of_death - $time_of_meal - $time_to_die)) ms."
         echo "Nb of died philosopher: [$nb_of_victim]"
  fi
+
  if [ $(( $time_of_death - $time_of_meal - $time_to_die)) -gt "10" ]; then
         echo "[ nop !]"
         exit 42
@@ -25,6 +45,7 @@ print_victim_last_meal (){
  else
         echo "[OK]"
  fi
+
  if [ $(( $nb_of_victim )) -ne "1" ]; then
         echo "[ nop !] more than one died"
         exit 42
@@ -32,13 +53,6 @@ print_victim_last_meal (){
 }
 
 main (){
- if [[ "$OSTYPE" == "darwin"* ]]; then
-       export SED_CMD="sed -e $'s/ /\\\n/g'"
-       echo "Macos .."
- else
-       export SED_CMD="sed 's/ /\n/g'"
-       echo "Linux .."
- fi
  ./philo_one $1 $2 $3 $4 > /tmp/a 2>/tmp/err ;
  var="fork" ; echo -n $var"      = " ;  cat /tmp/a | grep $var | wc -l ;
  var="eating" ; echo -n $var"    = " ;  cat /tmp/a | grep $var | wc -l ;
@@ -49,6 +63,8 @@ main (){
  echo "Death report:"
  var="died" ; cat /tmp/a | grep $var;
  print_victim_last_meal $2
+ echo "erro log:"
+ cat /tmp/err
 }
 
 echo "Parameters are: $1 $2 $3 $4"
